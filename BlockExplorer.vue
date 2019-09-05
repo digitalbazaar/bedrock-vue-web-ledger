271252734986554ad402f6e5a580b09f7255b945
David I. Lehn <dlehn@digitalbazaar.com>
2019-09-05 13:38:29 -0400

    Fix lint issues and cleanup.

--- BlockExplorer.vue
+++ BlockExplorer.vue
@@ -1,15 +1,23 @@
 <template>
-  <q-page class="row justify-center" padding>
-    <div class="column" style="width: 800px; max-width: 90vw;">
-      <h3 class="text-center">Block Explorer</h3>
+  <q-page
+    class="row justify-center"
+    padding>
+    <div
+      class="column"
+      style="width: 800px; max-width: 90vw;">
+      <h3 class="text-center">
+        Block Explorer
+      </h3>
       <blocks
         v-if="!loading"
-        :blockCache="blockCache"
-        :blockIdBase="blockIdBase"
-        :latestBlockHeight="latestBlockHeight"
-        :startBlock="startBlock"
-        :ledgerBlockService="ledgerBlockService" />
-      <div v-if="loading && showLoading" class="flex flex-center">
+        :block-cache="blockCache"
+        :block-id-base="blockIdBase"
+        :latest-block-height="latestBlockHeight"
+        :start-block="startBlock"
+        :ledger-block-service="ledgerBlockService" />
+      <div
+        v-if="loading && showLoading"
+        class="flex flex-center">
         <q-spinner-cube size="4em" />
       </div>
     </div>
@@ -29,6 +37,19 @@ const headers = {Accept: 'application/ld+json, application/json'};
 export default {
   name: 'BlockExplorer',
   components: {Blocks},
+  data() {
+    return {
+      loading: true,
+      showLoading: false,
+      // shared map from blockId to {block,meta}
+      // FIXME: use LRU cache?
+      blockCache: {},
+      blockIdBase: null,
+      latestBlockHeight: 0,
+      startBlock: 0,
+      ledgerBlockService: null
+    };
+  },
   async mounted() {
     const showLoadingId = setTimeout(() => this.showLoading = true, 1000);
     const laResponse = await axios.get('/ledger-agents/', {headers});
@@ -44,32 +65,14 @@ export default {
     this.blockCache[latestBlock.block.id] = latestBlock;
 
     this.blockIdBase = genesisBlock.block.id;
-    this.blockIdBase = this.blockIdBase.substring(0, this.blockIdBase.length - 1);
-
+    this.blockIdBase =
+      this.blockIdBase.substring(0, this.blockIdBase.length - 1);
 
     this.latestBlockHeight = latestBlock.block.blockHeight + 1;
-
-    //const blocks = [bsResponse.data.genesis];
-    //for(let i = 1; i <= this.latestBlockHeight; i++) {
-    //  blocks.push({block: {id: blockIdBase + i}});
-    //}
-    //this.blocks = blocks;
-
     this.ledgerBlockService = ledgerAgent.service.ledgerBlockService;
     this.startBlock = this.latestBlockHeight;
     clearTimeout(showLoadingId);
     this.loading = false;
-  },
-  data() {
-    return {
-      loading: true,
-      showLoading: false,
-      blockCache: {},
-      blockIdBase: null,
-      latestBlockHeight: 0,
-      startBlock: 0,
-      ledgerBlockService: null
-    };
   }
 };
 </script>
